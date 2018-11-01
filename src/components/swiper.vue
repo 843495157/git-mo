cea1d943d36497abe48c9baa026ddda8b44c5a35
Your <email@qq.com>
2018-11-01 17:22:30 +0800

    add 2

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,25 +1,38 @@
 <template>
-    <Carousel v-model="value1" :autoplay="setting.autoplay"
-    :autoplay-speed="setting.autoplaySpeed"
-    :dots="setting.dots"
-    :radius-dot="setting.radiusDot"
-    :trigger="setting.trigger"
-    :arrow="setting.arrow" 
-    loop>
+    <Carousel v-model="value1" :autoplay="setting.autoplay" :autoplay-speed="setting.autoplaySpeed" :dots="setting.dots"
+        :radius-dot="setting.radiusDot" :trigger="setting.trigger" :arrow="setting.arrow" loop>
         <CarouselItem>
-            <div class="demo-carousel"><img src="../images/lunbo-1.jpg" /></div>
+            <div class="demo-carousel"><img src="../images/1.jpg" /></div>
         </CarouselItem>
         <CarouselItem>
-            <div class="demo-carousel"><img src="../images/lunbo-2.jpg" /></div>
+            <div class="demo-carousel"><img src="../images/2.jpg" /></div>
         </CarouselItem>
         <CarouselItem>
-            <div class="demo-carousel"><img src="../images/lunbo-3.jpg" /></div>
+            <div class="demo-carousel"><img src="../images/3.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/4.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/5.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/6.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/7.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/8.jpg" /></div>
+        </CarouselItem>
+        <CarouselItem>
+            <div class="demo-carousel"><img src="../images/9.jpg" /></div>
         </CarouselItem>
     </Carousel>
 </template>
 <script>
     export default {
-        data () {
+        data() {
             return {
                 value1: 0,
                 setting: {
@@ -29,16 +42,16 @@
                     radiusDot: false,
                     trigger: 'click',
                     arrow: 'hover',
-                   
+
                 }
             }
         }
     }
 </script>
 <style scoped>
-    .demo-carousel img{
+    .demo-carousel img {
         width: 100%;
-        height: 600px;
+        height: 792px;
         cursor: pointer;
     }
 </style>
